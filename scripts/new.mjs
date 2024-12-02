b0d94e01f6f58f9a0d5eb913f42ac0bc5480bd68
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-02 10:35:17 -0500

    new args

--- scripts/new.mjs
+++ scripts/new.mjs
@@ -1,11 +1,24 @@
 import { mkdir, writeFile } from "fs/promises";
 import config from "../.env.json" with {type: "json"};
+import { parseArgs } from "util";
 
 const d = new Date();
-
-const year = d.getFullYear().toString().substring(2);
-
-const day = (d.getDay() + 1).toString();
+/**
+ * @type {import("util").ParseArgsConfig["options"]}
+ */
+const options = {
+    "year": {
+        type: "string",
+        default: d.getFullYear().toString().substring(2),
+        short: "y"
+    },
+    "day": {
+        type: "string",
+        default: (d.getDay() + 1).toString(),
+        short: "d"
+    }
+}
+const { year, day } = parseArgs({ args: process.argv.slice(2), options }).values;
 
 const url = `https://adventofcode.com/20${year}/day/${day}/input`;
 console.log(url);
