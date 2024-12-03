fda2836e3ce676f717f05cdce5ecdbef7ff6e015
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-02 20:57:19 -0500

    type arr to int

---
+++ utils/IsAny.ts
@@ -0,0 +1,2 @@
+type IsAny<T> = 1 extends (T & 2) ? true : false;
+export { IsAny as default };
--- utils/recursiveArrToInt.ts
+++ utils/recursiveArrToInt.ts
@@ -1,3 +1,7 @@
-export default function recursiveArrToInt(arr: any[]): any[] {
-    return arr.map((el) => Array.isArray(el) ? recursiveArrToInt(el) : parseFloat(el));
+import IsAny from "./IsAny";
+
+type test<T extends any[]> = IsAny<T> extends true ? T : T[number] extends any[] ? test<T[number]>[] : number[]
+
+export default function recursiveArrToInt<T extends any[]>(arr: T): test<T> {
+    return arr.map((el) => Array.isArray(el) ? recursiveArrToInt(el) : parseFloat(el)) as any; 
 }
